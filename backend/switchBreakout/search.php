59b136dcc23736c2c79c67e503531640877541da
emgul <31687258+emgul@users.noreply.github.com>
2017-10-16 13:53:19 +0200

    search function
    search

--- backend/switchBreakout/search.php
+++ backend/switchBreakout/search.php
@@ -1,13 +1,33 @@
 <?php
-$search = "låda";
+//$search = "låda";
 
-switch ($splitColumn) {
+switch ($dataAction) {
 	case 'searchAll':
-		$dbList = $db->list("products");
-		echo json_encode($dbList);
+        
+        
+        $testvar = "låda";
+        
+        
+		$dbList = $db->listJoined("products");
+        //echo json_encode($dbList[0]['id']);
+        
+        $arrayLength = count($dbList);
+        
+        $searchResult = array();
+        $x = 0;
+        while($x < $arrayLength){
+            if (in_array($testvar, $dbList[$x])) {
+                array_push($searchResult, $dbList[$x]);
+            }
+            $x++;
+        }
+        
+        echo json_encode($searchResult);
+        
 		break;
-
+        
+    default:
+		Echo "Invalid column type \n";
+		break;
 }
-
-
 ?>
