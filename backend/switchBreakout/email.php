4c57ddd9c6498655f4a10a9b3bb25ab1af82ed3e
emgul <31687258+emgul@users.noreply.github.com>
2017-10-19 14:56:40 +0200

    bilder

--- backend/switchBreakout/email.php
+++ backend/switchBreakout/email.php
@@ -1,53 +1,26 @@
 <?php
 
-
 switch ($dataAction) {
         
     case 'emailreceipt':
-         
         
         
-         $email = "t4.emil.henriksson@umea.nti.se";
-         $firstLastName = "John Doe";
+        $x = 3;
+        
+        while ($x < ){
+            
+        }
          
-         $to = $email;
-         $subject = "Tack för ditt köp ". $firstLastName;
-         
-         $x = 1;
-         $productammount = 5;
-         
-         $product1 = "låda";
-         $ammount ="4";
-         while($x <= 5) {
-             echo $x;
-             $x++;
-         }
-        echo "\r\n";
-         $message = "temporary";
+        $recipient="t4.emil.henriksson@umea.nti.se";
+        $subject="kvitto";
+        $mail_body="mail test";
 
-         // Always set content-type when sending HTML email
-         $headers = "MIME-Version: 1.0" . "\r\n";
-         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers = "From: boxshopreceipt@gmail.com" . "\r\n" .
+        "CC: boxshopreceipt@gmail.com";
 
-         // More headers
-         $headers .= 'From: <boxshopreceipt@gmail.com>' . "\r\n";
-         $headers .= 'Cc: boxshop@donotreply.com' . "\r\n";
-
-         mail($to,$subject,$message,$headers);
-     }
+    mail($recipient, $subject, $mail_body,$headers);
 
 
-
-
-/*
- $recipient="t4.emil.henriksson@umea.nti.se";
- $subject="kvitto";
- $mail_body="mail test";
-
-$headers = "From: boxshopreceipt@gmail.com" . "\r\n" .
-"CC: boxshopreceipt@gmail.com";
-
- mail($recipient, $subject, $mail_body,$headers);
-*/ 
+}
 
 ?>
--- backend/switchBreakout/search.php
+++ backend/switchBreakout/search.php
@@ -2,11 +2,18 @@
 //$search = "låda";
 
 switch ($dataAction) {
+        
+        
+
+        
+        
 	case 'searchAll':
         
-        
-        $testvar = "lå";
-        
+    if(isset($data[0]->search)){
+		$dataSearch = $data[0]->search;
+		$dataSearch = filter_var($dataSearch, FILTER_SANITIZE_STRIPPED, FILTER_FLAG_STRIP_LOW  );
+	}
+    
         
 		$dbList = $db->listJoined("products");
         //echo json_encode($dbList[0]['id']);
@@ -16,7 +23,7 @@ switch ($dataAction) {
         $searchResult = array();
         $x = 0;
         while($x < $arrayLength){
-            if (strpos($dbList[$x]['name'], $testvar) !==false || strpos($dbList[$x]['type'], $testvar) !==false) {
+            if (strpos($dbList[$x]['name'], $dataSearch) !==false || strpos($dbList[$x]['type'], $dataSearch) !==false) {
                 array_push($searchResult, $dbList[$x]);
             }
             
@@ -27,7 +34,7 @@ switch ($dataAction) {
             echo json_encode($searchResult);
         }
         else {
-            echo json_encode("Hittade inga resultat för " . $testvar);
+            echo json_encode("Hittade inga resultat för " . $dataSearch);
         }
 		break;
         
